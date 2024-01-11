eac83f4e2d0978c343099ab1fdf1e9521b97a326
emmanuel asakpa <emmanuelasakpa@yahoo.com>
2024-01-11 19:01:05 +0100

    js

---
+++ index.js
@@ -0,0 +1,33 @@
+// onclick="document.getElementById('my').src="jeremy-bishop-G9i_plbfDgk-unsplash.jpg""
+// onclick="document.getElementById('my').src="jeremy-bishop-G9i_plbfDgk-unsplash.jpg""
+ 
+
+
+ function boy(){
+    document.getElementById('cy').innerHTML='i am here';
+ }
+
+ function girl(){
+    document.getElementById('cy').innerHTML='daniel place';
+ }
+
+ function dada(){
+    document.getElementById('ey').innerHTML='update'
+ }
+
+//  document.write(2+2)
+ onclick='print'
+let x,y,z;
+x=2;
+y=3;
+z=x+y;
+
+document.getElementById('ty').innerHTML="the correct answer is " +z+ ".";
+
+
+let a,b,n;
+a=4;
+b=7;
+n=10+30;
+
+document.getElementById('ro').innerHTML=n;
